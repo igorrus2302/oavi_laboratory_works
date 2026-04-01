fc3cbf20af85ab11541d523268dd60062ed35460
Igor Grachev <149306755+igorrus2302@users.noreply.github.com>
2026-04-01 18:50:44 +0300

    updated images for lab4 and added random mode downloader

--- Lab4/lab4.py
+++ Lab4/lab4.py
@@ -3,6 +3,7 @@ import requests
 import numpy as np
 import cv2
 from PIL import Image
+import random
 
 
 origin = "https://www.slavcorpora.ru"
@@ -17,13 +18,39 @@ THRESHOLD = 100
 os.makedirs(INPUT_DIR, exist_ok=True)
 os.makedirs(OUTPUT_DIR, exist_ok=True)
 
-def download_images():
+def choose_mode():
+    print("Выберите режим загрузки изображений:")
+    print("1 — Последовательная загрузка")
+    print("2 — Случайная загрузка")
+
+    while True:
+        choice = input("Введите 1 или 2: ").strip()
+
+        if choice == "1":
+            return "sequential"
+        elif choice == "2":
+            return "random"
+        else:
+            print("Ошибка! Введите только 1 или 2.")
+
+def download_images(mode):
     sample = requests.get(f"{origin}/api/samples/{sample_id}").json()
-    pages = sample["pages"][:MAX_IMAGES]
+    pages = sample["pages"]
+
+    if mode == "sequential":
+        selected_pages = pages[:MAX_IMAGES]
+
+    elif mode == "random":
+        if MAX_IMAGES > len(pages):
+            raise ValueError("MAX_IMAGES больше, чем доступных изображений")
+        selected_pages = random.sample(pages, MAX_IMAGES)
+
+    else:
+        raise ValueError("Неверный режим загрузки")
 
     image_paths = []
 
-    for i, p in enumerate(pages):
+    for i, p in enumerate(selected_pages):
         url = f"{origin}/images/{p['filename']}"
         jpeg_path = os.path.join(INPUT_DIR, f"img_{i}.jpg")
 
@@ -77,14 +104,15 @@ def process_image(path, index):
     cv2.imwrite(f"{OUTPUT_DIR}/img_{index}_G.png", G_norm)
     cv2.imwrite(f"{OUTPUT_DIR}/img_{index}_binary.png", G_bin)
 
-
 def main():
-    paths = download_images()
+    mode = choose_mode()
+    paths = download_images(mode)
 
     for i, path in enumerate(paths):
         process_image(path, i)
 
     print("Успешно!")
+
 
 if __name__ == "__main__":
     main()
