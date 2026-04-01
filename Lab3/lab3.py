d8a68a42956f70f9dfd6e77e4b2f126333c1ddbd
Igor Grachev <149306755+igorrus2302@users.noreply.github.com>
2026-04-01 19:13:38 +0300

    updated lab3 images and added random mode

--- Lab3/lab3.py
+++ Lab3/lab3.py
@@ -2,6 +2,7 @@ import os
 import requests
 from PIL import Image
 import numpy as np
+import random
 
 
 origin = "https://www.slavcorpora.ru"
@@ -15,30 +16,57 @@ OUTPUT_DIR = "output_images"
 os.makedirs(INPUT_DIR, exist_ok=True)
 os.makedirs(OUTPUT_DIR, exist_ok=True)
 
-sample = requests.get(f"{origin}/api/samples/{sample_id}").json()
-image_paths = [f"{origin}/images/{p['filename']}" for p in sample["pages"]]
+def choose_mode():
+    print("Выберите режим загрузки изображений:")
+    print("1 — Последовательная загрузка")
+    print("2 — Случайная загрузка")
 
-image_paths = image_paths[:DOWNLOAD_LIMIT]
+    while True:
+        choice = input("Введите 1 или 2: ").strip()
 
-saved_files = []
+        if choice == "1":
+            return "sequential"
+        elif choice == "2":
+            return "random"
+        else:
+            print("Ошибка! Введите 1 или 2.")
 
-for i, url in enumerate(image_paths):
-    response = requests.get(url)
+def download_images(mode):
+    sample = requests.get(f"{origin}/api/samples/{sample_id}").json()
+    pages = sample["pages"]
 
-    jpg_path = os.path.join(INPUT_DIR, f"img_{i}.jpg")
-    png_path = os.path.join(INPUT_DIR, f"img_{i}.png")
+    if mode == "sequential":
+        selected_pages = pages[:DOWNLOAD_LIMIT]
 
-    with open(jpg_path, "wb") as f:
-        f.write(response.content)
+    elif mode == "random":
+        if DOWNLOAD_LIMIT > len(pages):
+            raise ValueError("DOWNLOAD_LIMIT больше количества доступных изображений")
+        selected_pages = random.sample(pages, DOWNLOAD_LIMIT)
 
-    img = Image.open(jpg_path).convert("L")
-    img.save(png_path, "PNG")
+    else:
+        raise ValueError("Неверный режим")
 
-    os.remove(jpg_path)
+    saved_files = []
 
-    saved_files.append(png_path)
+    for i, p in enumerate(selected_pages):
+        url = f"{origin}/images/{p['filename']}"
 
-print("Скачивание и перевод завершены")
+        jpg_path = os.path.join(INPUT_DIR, f"img_{i}.jpg")
+        png_path = os.path.join(INPUT_DIR, f"img_{i}.png")
+
+        response = requests.get(url)
+
+        with open(jpg_path, "wb") as f:
+            f.write(response.content)
+
+        img = Image.open(jpg_path).convert("L")
+        img.save(png_path, "PNG")
+
+        os.remove(jpg_path)
+
+        saved_files.append(png_path)
+
+    return saved_files
 
 def median_cross_filter(img_array):
     h, w = img_array.shape
@@ -60,27 +88,37 @@ def median_cross_filter(img_array):
 
     return result * 255
 
-print("Фильтрация")
+def main():
+    mode = choose_mode()
 
-for path in saved_files:
-    filename = os.path.basename(path).replace(".png", "")
+    saved_files = download_images(mode)
 
-    img = Image.open(path).convert("L")
-    arr = np.array(img)
+    print("Скачивание и перевод завершены")
+    print("Фильтрация")
 
-    filtered = median_cross_filter(arr)
+    for path in saved_files:
+        filename = os.path.basename(path).replace(".png", "")
 
-    binary_original = (arr > 127).astype(np.uint8)
-    binary_filtered = (filtered > 127).astype(np.uint8)
+        img = Image.open(path).convert("L")
+        arr = np.array(img)
 
-    diff = np.bitwise_xor(binary_original, binary_filtered) * 255
+        filtered = median_cross_filter(arr)
 
-    Image.fromarray(filtered.astype(np.uint8)).save(
-        os.path.join(OUTPUT_DIR, f"{filename}_filtered.png")
-    )
+        binary_original = (arr > 127).astype(np.uint8)
+        binary_filtered = (filtered > 127).astype(np.uint8)
 
-    Image.fromarray(diff.astype(np.uint8)).save(
-        os.path.join(OUTPUT_DIR, f"{filename}_diff.png")
-    )
+        diff = np.bitwise_xor(binary_original, binary_filtered) * 255
 
-print("Успешно")
+        Image.fromarray(filtered.astype(np.uint8)).save(
+            os.path.join(OUTPUT_DIR, f"{filename}_filtered.png")
+        )
+
+        Image.fromarray(diff.astype(np.uint8)).save(
+            os.path.join(OUTPUT_DIR, f"{filename}_diff.png")
+        )
+
+    print("Успешно")
+
+
+if __name__ == "__main__":
+    main()
